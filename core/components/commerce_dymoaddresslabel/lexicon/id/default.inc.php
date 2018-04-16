df020af09db2c8c9e8921ae75e89f599d725655f
Mark Hamstra <hello@markhamstra.com>
2018-04-16 22:19:59 +0200

    Lexicon sync

---
+++ core/components/commerce_dymoaddresslabel/lexicon/id/default.inc.php
@@ -0,0 +1,4 @@
+<?php
+
+$_lang['commerce_dymoaddresslabel'] = 'Dymo Address Label';
+$_lang['commerce_dymoaddresslabel.description'] = 'Integrates with the Dymo Label Writer SDK to print address labels directly from the order view.';
---
+++ core/components/commerce_dymoaddresslabel/lexicon/nl/default.inc.php
@@ -0,0 +1,4 @@
+<?php
+
+$_lang['commerce_dymoaddresslabel'] = 'Dymo Adreslabel';
+$_lang['commerce_dymoaddresslabel.description'] = 'Integreert met de Dymo Label Writer SDK om het mogelijk te maken om adreslabels direct vanaf de bestellingpagina af te drukken.';
