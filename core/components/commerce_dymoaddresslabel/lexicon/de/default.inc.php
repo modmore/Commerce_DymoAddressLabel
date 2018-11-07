d52952c119e2736e37eb0f386936f8e3ccb56152
Mark Hamstra <hello@markhamstra.com>
2018-11-07 14:02:31 +0100

    Sync translations

--- core/components/commerce_dymoaddresslabel/lexicon/de/default.inc.php
+++ core/components/commerce_dymoaddresslabel/lexicon/de/default.inc.php
@@ -4,5 +4,5 @@ $_lang['commerce_dymoaddresslabel'] = 'DYMO-Adressetikett';
 $_lang['commerce_dymoaddresslabel.description'] = 'Integration mit dem Dymo Label Writer SDK, um Adressaufkleber direkt aus der Bestellansicht zu drucken.';
 $_lang['commerce_dymoaddresslabel.sdk_version'] = 'JS SDK Version';
 $_lang['commerce_dymoaddresslabel.sdk_version.desc'] = 'What version of the JavaScript SDK to use. When you use Dymo Label Software 8.7.2 or above, use 3.0, otherwise, use the legacy version. If you choose the wrong version, the print label button will not be available.';
-$_lang['commerce_dymoaddresslabel.sdk_version.legacy'] = 'Legacy (DLS before 8.7.2)';
-$_lang['commerce_dymoaddresslabel.sdk_version.3.0'] = 'v3.0 (DLS 8.7.2 or newer)';
+$_lang['commerce_dymoaddresslabel.sdk_version.legacy'] = 'Legacy (DLS bevor 8.7.2)';
+$_lang['commerce_dymoaddresslabel.sdk_version.3.0'] = 'v3.0 (DLS 8.7.2 oder neuer)';
