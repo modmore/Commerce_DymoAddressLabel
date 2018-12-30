d719147f85ed2fda6a84064183bff68a03773fad
Mark Hamstra <hello@markhamstra.com>
2018-12-30 21:27:54 +0100

    Lexicon sync

--- core/components/commerce_dymoaddresslabel/lexicon/it/default.inc.php
+++ core/components/commerce_dymoaddresslabel/lexicon/it/default.inc.php
@@ -1,8 +1,8 @@
 <?php
 
-$_lang['commerce_dymoaddresslabel'] = 'Dymo Address Label';
-$_lang['commerce_dymoaddresslabel.description'] = 'Integrates with the Dymo Label Writer SDK to print address labels directly from the order view.';
-$_lang['commerce_dymoaddresslabel.sdk_version'] = 'JS SDK Version';
-$_lang['commerce_dymoaddresslabel.sdk_version.desc'] = 'What version of the JavaScript SDK to use. When you use Dymo Label Software 8.7.2 or above, use 3.0, otherwise, use the legacy version. If you choose the wrong version, the print label button will not be available.';
+$_lang['commerce_dymoaddresslabel'] = 'Dymo Etichette';
+$_lang['commerce_dymoaddresslabel.description'] = 'Si integra con la Dymo Label Writer SDK per stampare etichette per indirizzi direttamente dalla visualizzazione ordine.';
+$_lang['commerce_dymoaddresslabel.sdk_version'] = 'Versione SDK JS';
+$_lang['commerce_dymoaddresslabel.sdk_version.desc'] = 'Quale versione di JavaScript SDK utilizzare. Quando si utilizza il Software Dymo Label 8.7.2 o sopra, utilizzare la versione 3.0, in caso contrario, utilizzare la versione precedente. Se si sceglie la versione errata, è possibile che il pulsante Stampa etichetta non sia disponibile.';
 $_lang['commerce_dymoaddresslabel.sdk_version.legacy'] = 'Legacy (DLS before 8.7.2)';
-$_lang['commerce_dymoaddresslabel.sdk_version.3.0'] = 'v3.0 (DLS 8.7.2 or newer)';
+$_lang['commerce_dymoaddresslabel.sdk_version.3.0'] = 'v 3.0 (DLS 8.7.2 o più recente)';
